c7d636533b91f84e6ae5d654bf5aff291219a526
William <wlindb@kth.se>
2021-08-11 19:20:07 +0200

    Working on location

--- GeoFeed/app/src/main/java/se/umu/student/wili0037/geofeed/activities/MainActivity.kt
+++ GeoFeed/app/src/main/java/se/umu/student/wili0037/geofeed/activities/MainActivity.kt
@@ -1,13 +1,20 @@
 package se.umu.student.wili0037.geofeed.activities
 
+import android.content.pm.PackageManager
 import android.location.Geocoder
+import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.gms.location.FusedLocationProviderClient
+import com.google.android.gms.location.LocationServices
+import com.google.android.gms.tasks.Task
 import se.umu.student.wili0037.geofeed.R
 import se.umu.student.wili0037.geofeed.activities.adapters.RecyclerAdapter
 import kotlin.random.Random
@@ -18,17 +25,49 @@ class MainActivity : AppCompatActivity() {
     private var titleList = mutableListOf<String>()
     private var descList = mutableListOf<String>()
     private var imageList = mutableListOf<Int>()
+    private var cityName = "Unknown city"
+
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private lateinit var geocoder: Geocoder
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        val actionBar = supportActionBar
-        actionBar?.subtitle = "Stockholm"
+
+        fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
+        geocoder = Geocoder(this)
+        getLastLocation()
         postToList()
         val rv_recyclerView: RecyclerView = findViewById(R.id.rv_recyclerView) as RecyclerView
         rv_recyclerView.apply {
             layoutManager = LinearLayoutManager(context)
             adapter = RecyclerAdapter(titleList, descList, imageList)
+        }
+    }
+
+    private fun updateSubTitle(cityName: String) {
+        val actionBar = supportActionBar
+        actionBar?.subtitle = cityName
+    }
+    
+
+    private fun getLastLocation() {
+        val locationTask = fusedLocationProviderClient.lastLocation
+
+        if(ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+            && ActivityCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION), 101)
+            return
+        }
+        locationTask.addOnSuccessListener { location ->
+            if(location == null) return@addOnSuccessListener
+
+            val addresses = geocoder.getFromLocation(location.latitude, location.longitude, 10)
+            val address = addresses.find { address -> address.locality != null }
+            if(address != null) {
+                Log.d("location", "${address.locality}")
+                updateSubTitle(address.locality)
+            }
         }
     }
 
