1bd3a78099fa8471694f83e8f9a480ffe509896c
William <wlindb@kth.se>
2021-08-13 14:12:44 +0200

    Moved API to cloud

--- GeoFeed/app/src/main/java/se/umu/student/wili0037/geofeed/utils/Constants.kt
+++ GeoFeed/app/src/main/java/se/umu/student/wili0037/geofeed/utils/Constants.kt
@@ -3,6 +3,6 @@ package se.umu.student.wili0037.geofeed.utils
 class Constants {
 
     companion object {
-        const val BASE_URL = "http://10.0.2.2:3000"
+        const val BASE_URL = "https://geofeedapi.nw.r.appspot.com"
     }
 }
