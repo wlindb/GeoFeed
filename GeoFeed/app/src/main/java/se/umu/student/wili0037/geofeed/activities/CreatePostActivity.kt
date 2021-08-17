7b8fec1b1dae3609fe1db003ed20ff60c3d14286
William <wlindb@kth.se>
2021-08-17 14:11:38 +0200

    Working on creating new posts

--- GeoFeed/app/src/main/java/se/umu/student/wili0037/geofeed/activities/CreatePostActivity.kt
+++ GeoFeed/app/src/main/java/se/umu/student/wili0037/geofeed/activities/CreatePostActivity.kt
@@ -2,18 +2,24 @@ package se.umu.student.wili0037.geofeed.activities
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.ActionMode
+import android.view.Menu
 import android.view.MenuItem
+import android.widget.Toast
+import androidx.lifecycle.ViewModelProvider
+import com.google.android.material.textfield.TextInputEditText
 import se.umu.student.wili0037.geofeed.R
 
 class CreatePostActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_create_post)
-        
         val actionBar = supportActionBar
-        actionBar?.setDisplayHomeAsUpEnabled(true)
+        actionBar?.title = "Create Post"
+        //actionBar?.setDisplayHomeAsUpEnabled(true)
+        val input : TextInputEditText = findViewById(R.id.tiet_edittext)
     }
-
+    /*
     override fun onContextItemSelected(item: MenuItem): Boolean {
         when (item.itemId) {
             android.R.id.home -> {
@@ -22,5 +28,18 @@ class CreatePostActivity : AppCompatActivity() {
             }
         }
         return super.onContextItemSelected(item)
+    }
+    */
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.create_post_action_bar, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.submit -> Toast.makeText(this, "Submit Clicked", Toast.LENGTH_SHORT).show()
+        }
+        return super.onOptionsItemSelected(item)
     }
 }
